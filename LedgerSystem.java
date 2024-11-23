ef5c453c96c901a010a1673cbf37e8d3066986ed
fathihamdan <irfanfathi.hamdan@gmail.com>
2024-11-23 23:08:03 +0800

    Update LedgerSystem.java

--- LedgerSystem.java
+++ LedgerSystem.java
@@ -1,1 +1,14 @@
 
+import java.util.Scanner;
+
+public class LedgerSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while(true){
+            System.out.print("Please enter your request:");
+            int a = sc.nextInt();
+        }
+        
+    }
+}
