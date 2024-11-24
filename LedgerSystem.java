4d94499c7732a052b2faef419f804ac41aac65da
fathihamdan <irfanfathi.hamdan@gmail.com>
2024-11-24 21:22:45 +0800

    Update LedgerSystem.java

--- LedgerSystem.java
+++ LedgerSystem.java
@@ -6,13 +6,32 @@ public class LedgerSystem {
         Scanner sc = new Scanner(System.in);
         
         while(true){
-            System.out.print("== Transaction ==");
-            System.out.print("1. Debit\n"
+            System.out.println("== Transaction ==");
+            System.out.println("1. Debit\n"
                            + "2. Credit\n"
                            + "3. History\n"
                            + "4. Savings\n"
                            + "5. Credit loan");
-            int a = sc.nextInt();
+            int option = sc.nextInt();
+            
+            switch (option){
+                case 1:
+                    System.out.println("== Debit ==");
+                    System.out.println("Enter amount:");
+                    sc.nextDouble();
+                    break;
+                    
+                
+                case 2:
+                    
+                case 3:
+                    
+                case 4:
+                    
+                case 5:
+                    
+            }
+            
         }
         
     }
