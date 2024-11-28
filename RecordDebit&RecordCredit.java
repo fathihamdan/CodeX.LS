a4e9a920520ba3cbf0bbfbf1816c9c8aeecd6eb9
AlisyaAida <alisyaaida785@gmail.com>
2024-11-29 01:09:53 +0800

    Update LedgerSystem.java

--- RecordDebit&RecordCredit.java
+++ RecordDebit&RecordCredit.java
@@ -1,63 +1,165 @@
-
 import java.util.Scanner;
-
 public class LedgerSystem {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        
+        //User Registration Validation
+        String regName=" ";
+        String regEmail=" ";
+        String regPass=" ";
+        String regEmailValid=" ";
+        String regPassValid=" ";
+        
+        while(true){
+            System.out.println("\n== Ledger System ==");
+            System.out.println("Login or Register");
+            System.out.println("1.Login");
+            System.out.println("2.Register");
+            System.out.print("\n>");
+            int LogReg=sc.nextInt();
+            
+            sc.nextLine();
+        
+            if (LogReg==2){
+                System.out.println("\n== Please fill in the form ==");
+                System.out.print("Name: ");
+                regName=sc.nextLine();
+                
+                while(true){
+                    System.out.print("Email: ");
+                    regEmail=sc.nextLine();
+                
+                    if(regEmail.endsWith("@gmail.com")){
+                        regEmailValid=regEmail;
+                        break;
+                    }
+                
+                    else{
+                        System.out.println("\nEmail must be in correct format(name@gmail.com).\n");
+                    }
+                } 
+                
+                while(true){
+                    System.out.print("Password: ");
+                    regPass=sc.nextLine();
+                    
+                    if (regPass.length()<8){
+                        System.out.println("\nPassword must be at least 8 characters.\n");
+                    }
+                    
+                    boolean letter=false;
+                    boolean digit=false;
+                    boolean spec=false;
+                    
+                    for(int i=0; i<regPass.length(); i++){
+                        char charac=regPass.charAt(i);
+                        
+                        if (Character.isLetter(charac))
+                            letter=true;
+                        else if (Character.isDigit(charac))
+                            digit=true;
+                        else if (!Character.isLetterOrDigit(charac))
+                            spec=true;
+                    }
+                    
+                    if(letter&&digit&&spec){
+                        regPassValid=regPass;
+                        System.out.println("\nRegister succesful!!!\n");
+                        break;
+                    }
+                    
+                    else{
+                        System.out.println("\nPassword must contain at least one letter, digit and special character.\n");
+                    }
+                }
+            }
+            
+            else if (LogReg==1){
+                System.out.println("\n== Please enter your email and password ==");
+                
+                while(true){
+                    System.out.print("Email: ");
+                    String email=sc.nextLine();
+                    System.out.print("Password: ");
+                    String pass=sc.nextLine();
+                
+                    if(email.equals(regEmailValid)&&pass.equals(regPassValid)){
+                        System.out.println("\nLogin Successful!!!\n");
+                        break;
+                    }
+                
+                    else{
+                        System.out.println("\nYour email or password is wrong.");
+                    }
+                }
+                break;
+            }
+            else{
+                System.out.println("\nPlease choose between 1 or 2 only.");
+            }
+        }
+        
         Double []debit=new Double[100];
         String []descDebit=new String[100];
         int count=0;
         boolean running = true;
+        
+        while(true){
+            System.out.println("\n== Welcome, "+regName+" ==");
+            System.out.println("Balance: "+debit[count]);
+            System.out.println("Savings: ");
+            System.out.println("Loan: ");
 
-        while(running){
+            while(running){
             
-            System.out.println("== Transaction ==");
-            System.out.println("1. Debit\n"
-                           + "2. Credit\n"
-                           + "3. History\n"
-                           + "4. Savings\n"
-                           + "5. Credit loan\n"
-                           + "6. Deposit Interest Predictor\n"
-                           + "7. Logout");
-            int option = sc.nextInt();
+                System.out.println("\n== Transaction ==");
+                System.out.println("1. Debit\n"
+                               + "2. Credit\n"
+                               + "3. History\n"
+                               + "4. Savings\n"
+                               + "5. Credit loan\n"
+                               + "6. Deposit Interest Predictor\n"
+                               + "7. Logout");
+                System.out.print("\n>");
+                int option = sc.nextInt();
             
-            switch (option){
-                case 1:
-                    System.out.println("== Debit ==");
-                    System.out.print("Enter amount:");
-                    debit[count]=sc.nextDouble();
-                    sc.nextLine();
-                    System.out.print("Enter description: ");
-                    descDebit[count]=sc.nextLine();
-                    System.out.println(descDebit[count]);
-                    count++;
-
-                    System.out.println("Debit Successfully Recorded!!!");
-                    break;
+                switch (option){
+                    case 1:
+                        System.out.println("\n== Debit ==");
+                        System.out.print("Enter amount: ");
+                        debit[count]=sc.nextDouble();
+                        sc.nextLine();
+                        System.out.print("Enter description: ");
+                        descDebit[count]=sc.nextLine();
+                        System.out.println(descDebit[count]);
+                        count++;
+                        
+                        System.out.println("\nDebit Successfully Recorded!!!\n");
+                        break;
                     
                 
-                case 2:
+                    case 2:
                     
-                case 3:
+                    case 3:
                     
-                case 4:
+                    case 4:
                     
-                case 5:
+                    case 5:
                     
-                case 6:
+                    case 6:
 
-                case 7:
-                    System.out.println("Thank you for using Ledger System!");
-                    running = false;
-                    break;
+                    case 7:
+                        System.out.println("Thank you for using Ledger System!");
+                        running = false;
+                        break;
 
-                default:
-                    System.out.println("Invalid option. Please try again.");
-                    break;
-            }
-            
-            
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                        break;
+                }
+            break;
+            }   
         }
-        
     }
 }
