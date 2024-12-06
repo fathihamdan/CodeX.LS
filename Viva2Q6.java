985b91e4581bae44d5b2211f10fa894bc3438014
fathihamdan <irfanfathi.hamdan@gmail.com>
2024-12-07 03:11:47 +0800

    Update Viva2Q6.java

--- Viva2Q6.java
+++ Viva2Q6.java
@@ -8,37 +8,42 @@ import java.util.Scanner;
  *
  * @author irfan
  */
-public class Question6 {
-    
+import java.util.Scanner;
+
+public class Viva2Question6 {
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
         String notName = scanner.nextLine();
-
-        String realName = notName.replaceAll("[^a-zA-Z0-9]", " ");//replace every single char that are not a-z, A-Z, 0,9
-                                                                 //^symbol means other than
-        realName = realName.replaceAll("\\b(BIN |bin |BINTI |binti |A/L |a/l |AL |al |A/P |a/p |AP |ap |VAN |van |VON |von )\\b", "");
+        
+        String realName = notName.replaceAll("\\b(BIN |bin |BINTI |binti |A/L |a/l |AL |al |A/P |a/p |AP |ap |VAN |van |VON |von )\\b", "");
+        realName = realName.replaceAll("[^a-zA-Z0-9]", " ");  //replace every single char that are not a-z, A-Z, 0,9
+                                                                                //^symbol means other than
+        realName = realName.toUpperCase();
         String startTime = scanner.nextLine();
         String endTime = scanner.nextLine();
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        
-        generateInitials(realName);
-        
-        isPrintingWelcomeMessage(realName);
-        
-        calculateInterval(startTime, endTime);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         
 
+        if(isPrintingWelcomeMessage(realName)==true){
+            System.out.println("Welcome to G101, Kolej Kediaman Kinabalu, University of Malaya");
+        }
+
+        generateInitials(realName);
         
+        calculateInterval(startTime, endTime);
+         
+        System.out.println("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
     }
     public static void generateInitials(String realName){
-        realName = realName.toUpperCase();
-        
         if(realName.contains("LEE")&&realName.contains("KAH")&&realName.contains("SING")){
             realName = ("LEE KAH SING");
             System.out.println ("LKS!!!!!!!!!!");
+            System.out.println("WE KNOW IT'S YOU -- "+realName);
+            System.out.println("SLEEP NOW!!!!!!!!!!");
             
         }
-        else if(realName.contains("RIDWAN")&&realName.contains("FAIZ")&&realName.contains("MOHAMMAD")&&realName.contains("HASSAN")){
+        else if(realName.contains("RIDWAN")&&realName.contains("FAIZ")&&realName.contains("MOHAMAD")&&realName.contains("HASSAN")){
             realName = ("RIDWAN FAIZ MOHAMAD HASSAN");
             System.out.println ("RFMH");
         }
@@ -49,7 +54,6 @@ public class Question6 {
         else{
             
             String[] seperate = realName.toUpperCase().split(" ");
-            System.out.println(realName);
             String initial = "";
             for(int i=0;i<seperate.length;i++){
                 initial+=seperate[i].charAt(0);
@@ -59,9 +63,12 @@ public class Question6 {
         }
         
     }
-    public static void isPrintingWelcomeMessage(String realName){
+    public static boolean isPrintingWelcomeMessage(String realName){
         if(realName.equals("LEE KAH SING")||realName.equals("RIDWAN FAIZ MOHAMAD HASSAN")||realName.equals("SURESH SUBRAMANIAM")){
-            System.out.println("Welcome to G101, Kolej Kediaman Kinabalu, University of Malaya");
+            return true;
+        }
+        else{
+            return false;
         }
     }
     public static void calculateInterval(String startTime,String endTime){
@@ -83,3 +90,4 @@ public class Question6 {
         System.out.printf("%02d:%02d:%02d",HH,MM,SS);
     }
 }
+
