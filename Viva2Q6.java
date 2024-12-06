6b43d21bd614dc67741cac55b7a62935c76ed39d
fathihamdan <irfanfathi.hamdan@gmail.com>
2024-12-07 03:13:03 +0800

    Update Viva2Q6.java

--- Viva2Q6.java
+++ Viva2Q6.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.viva2;
-import java.util.Scanner;
-/**
- *
- * @author irfan
- */
 import java.util.Scanner;
 
 public class Viva2Question6 {
